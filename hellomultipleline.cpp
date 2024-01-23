0004e1541d2c6bc8d108205c4dded83a2a4f6c4e
goldpikablue123 <157506185+goldpikablue123@users.noreply.github.com>
2024-01-23 09:53:09 -0600

    Add files via upload

---
+++ hellomultipleline.cpp
@@ -0,0 +1,23 @@
+// Mutliple line hello, By Paxton Orchar
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int choices = 0;
+    cout << "The Past never dies." << endl;
+    cout << "And the past always turns up tails." << endl;
+    cout << "But theres always a future to walk towards." << endl;
+    cout << "So what will you do? Continue? Press 1 to try again." << endl;
+    cin >> choices;
+    if (choices = 1)
+    {
+        cout << "Good luck Stardust.";
+        else
+        {
+            cout << "Then goodbye. Forever.";
+        }
+    }
+}
