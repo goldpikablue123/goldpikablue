710053b54ec178aca5be086d7e056c2fdce45298
goldpikablue123 <157506185+goldpikablue123@users.noreply.github.com>
2024-01-23 10:03:02 -0600

    Add files via upload

---
+++ choice-1.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    // variables
+    int userChoice = 0;
+
+    cout << "Press 1 to choose Stars. Press 2 to choose Hell .";
+    cin >> userChoice;
+
+    switch (userChoice)
+        ;
+    {
+    case 1:
+        cout << "The stars are beaut" break;
+
+    default:
+        break;
+    }
+}
